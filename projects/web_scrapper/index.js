d33eedac8e9d35d94d554190d250773a30fb700f
Divyansh <divyanshmoonat@outlook.com>
2024-06-24 22:01:31 +0530

    24-06-2024 | Web Scrapping - Day 2

--- projects/web_scrapper/index.js
+++ projects/web_scrapper/index.js
@@ -1,16 +1,82 @@
 const axios = require("axios");
+const cheerio = require("cheerio");
+const xlsx = require("xlsx");
 
-const pageUrl =
-  "https://www.amazon.com/s?k=phone&page=2&crid=18EUYBSP7O1SQ&qid=1702535235&sprefix=phon%2Caps%2C280&ref=sr_pg_2";
+const fs = require("fs");
+
+const pageUrl = "https://www.naukri.com/it-jobs?src=gnbjobs_homepage_srch";
+
+// fetch(pageUrl, {
+//   headers: {
+//     "Content-Type": "text/html",
+//   },
+// })
+//   .then((res) => res.text())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log("ERR", err));
 
 const getPageData = async () => {
   try {
-    const response = await axios.get(pageUrl);
-    const data = response.data;
+    const response = await axios.get(pageUrl, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+    const data = response.data; // HTML Data
     console.log(data);
+
+    // const pageData = fs.readFileSync("pagedata.txt");
+    // console.log(pageData.toString());
+    const pageData = data;
+    const $ = cheerio.load(pageData.toString());
+    const titles = $(".a-size-medium.a-color-base.a-text-normal"); // Input -> document.querySelector("TARGETING INFORMATION")
+
+    const titlesData = [];
+
+    titles.each((index, element) => {
+      const title = $(element).text();
+      titlesData.push(title);
+      // products.push({
+      //   name: title,
+      // });
+    });
+
+    const pricesData = [];
+
+    const prices = $(".a-price-whole");
+    // console.log(prices);
+    prices.each((index, element) => {
+      const price = $(element).text();
+      // console.log(price);
+      pricesData.push(price);
+    });
+
+    // console.log(titlesData);
+    // console.log(pricesData);
+
+    const productsJson = titlesData.map((title, index) => {
+      return {
+        title,
+        price: pricesData[index],
+      };
+    });
+
+    // console.log(productsJson);
+
+    // fs.writeFileSync("products.json", JSON.stringify(productsJson));
+
+    // Excel
+
+    const workbook = xlsx.utils.book_new();
+    const sheet = xlsx.utils.json_to_sheet(productsJson);
+
+    xlsx.utils.book_append_sheet(workbook, sheet, "Products");
+    xlsx.writeFile(workbook, "products.xlsx");
+
+    console.log("XLSX file created successfully!");
   } catch (err) {
-    console.log(err);
+    console.log("ERR RETREIVING DATA", err);
   }
 };
 
-getPageData();
+// getPageData();
--- projects/web_scrapper/readme.md
+++ projects/web_scrapper/readme.md
@@ -1,2 +1,3 @@
 Steps
 1. Get the data of the page using axios
+2. Scan the data and treat it as HTML DOM
