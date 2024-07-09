f28b3c2d0f529cf72507a0887f3c4406fae1c4d1
Divyansh <divyanshmoonat@outlook.com>
2024-07-09 20:14:00 +0530

    Adding Cloud MongoDB URL

--- projects/job_app/index.js
+++ projects/job_app/index.js
@@ -6,7 +6,7 @@ const jobRoutes = require("./route/job");
 const app = express();
 
 mongoose
-  .connect("mongodb://localhost:27017/job_app")
+  .connect("mongodb+srv://djain7429:4PVCk0BAwPRFbGnj@cluster0.xomtfof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("DB Connected successfully"))
   .catch((err) => console.log("Error connecting database", err));
 
